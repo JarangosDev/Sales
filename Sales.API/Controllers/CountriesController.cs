36d8d0d43c17d452c8bbee39fcf260befe0b4ed5
Jarangos <94191961+Jarangos31@users.noreply.github.com>
2023-03-10 12:00:52 -0500

    App Pagination and Filter

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Sales.API.Data;
+using Sales.API.Helpers;
+using Sales.Shared.DTOs;
 using Sales.Shared.Entities;
 
 namespace Sales.API.Controllers
@@ -17,12 +18,39 @@ namespace Sales.API.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAsync()
+        public async Task<IActionResult> GetAsync([FromQuery] PaginationDTO pagination)
         {
-            return Ok(await _context.Countries
-                .Include(x => x.States)
+
+            var queryable = _context.Countries
+            .Include(x => x.States)
+            .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
+
+            return Ok(await queryable
+                .OrderBy(x => x.Name)
+                .Paginate(pagination)
                 .ToListAsync());
         }
+
+        [HttpGet("totalPages")]
+        public async Task<ActionResult> GetPages([FromQuery] PaginationDTO pagination)
+        {
+            var queryable = _context.Countries.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+            double count = await queryable.CountAsync();
+            double totalPages = Math.Ceiling(count / pagination.RecordsNumber);
+            return Ok(totalPages);
+        }
+
+
         [HttpGet("full")]
         public async Task<IActionResult> GetFullAsync()
         {
@@ -33,7 +61,7 @@ namespace Sales.API.Controllers
         }
 
         [HttpGet("{id:int}")]
-        public async Task<IActionResult> GetAsync( int id)
+        public async Task<IActionResult> GetAsync(int id)
         {
             var country = await _context.Countries
                 .Include(x => x.States!)
@@ -62,7 +90,8 @@ namespace Sales.API.Controllers
                     return BadRequest("Ya existe un país con el mismo nombre.");
                 }
                 return BadRequest(dbUpdateException.Message);
-            }catch(Exception exception)
+            }
+            catch (Exception exception)
             {
                 return BadRequest(exception.Message);
             }
