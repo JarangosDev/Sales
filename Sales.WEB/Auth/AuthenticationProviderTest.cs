bf2f75b40edb57b2ba956ee155dd7b392f40c44d
Jarangos <94191961+Jarangos31@users.noreply.github.com>
2023-03-15 15:14:04 -0500

    Security Harcode and Web.

---
+++ Sales.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var jarangosUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Jair"),
+                new Claim("LastName", "Arango"),
+                new Claim(ClaimTypes.Name, "jarangosdev@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            },
+            authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(jarangosUser)));
+        }
+    }
+}
