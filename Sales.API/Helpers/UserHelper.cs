2fdce4bd7463d19b87433b7b5334488daf0fb371
Jarangos <94191961+Jarangos31@users.noreply.github.com>
2023-03-27 18:43:25 -0500

    Changing user password.

--- Sales.API/Helpers/UserHelper.cs
+++ Sales.API/Helpers/UserHelper.cs
@@ -11,7 +11,7 @@ namespace Sales.API.Helpers
         private readonly DataContext _context;
         private readonly UserManager<User> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
-        private readonly SignInManager<User> _signInManager;    
+        private readonly SignInManager<User> _signInManager;
 
         public UserHelper(DataContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager, SignInManager<User> signInManager)
         {
@@ -19,6 +19,16 @@ namespace Sales.API.Helpers
             _userManager = userManager;
             _roleManager = roleManager;
             _signInManager = signInManager;
+        }
+
+        public async Task<string> GenerateEmailConfirmationTokenAsync(User user)
+        {
+            return await _userManager.GenerateEmailConfirmationTokenAsync(user);
+        }
+
+        public async Task<IdentityResult> ConfirmEmailAsync(User user, string token)
+        {
+            return await _userManager.ConfirmEmailAsync(user, token);
         }
 
         public async Task<IdentityResult> AddUserAsync(User user, string password)
@@ -49,9 +59,28 @@ namespace Sales.API.Helpers
                 .Include(u => u.City!)
                 .ThenInclude(c => c.State!)
                 .ThenInclude(s => s.Country!)
-                .FirstOrDefaultAsync(u => u.Email! == email);
+                .FirstOrDefaultAsync(x => x.Email == email);
             return user!;
+        }
 
+        public async Task<User> GetUserAsync(Guid userId)
+        {
+            var user = await _context.Users
+                .Include(u => u.City!)
+                .ThenInclude(c => c.State!)
+                .ThenInclude(s => s.Country!)
+                .FirstOrDefaultAsync(x => x.Id == userId.ToString());
+            return user!;
+        }
+
+        public async Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword)
+        {
+            return await _userManager.ChangePasswordAsync(user, currentPassword, newPassword);
+        }
+
+        public async Task<IdentityResult> UpdateUserAsync(User user)
+        {
+            return await _userManager.UpdateAsync(user);
         }
 
         public async Task<bool> IsUserInRoleAsync(User user, string roleName)
@@ -69,5 +98,14 @@ namespace Sales.API.Helpers
             await _signInManager.SignOutAsync();
         }
 
+        public async Task<string> GeneratePasswordResetTokenAsync(User user)
+        {
+            return await _userManager.GeneratePasswordResetTokenAsync(user);
+        }
+
+        public async Task<IdentityResult> ResetPasswordAsync(User user, string token, string password)
+        {
+            return await _userManager.ResetPasswordAsync(user, token, password);
+        }
     }
 }
