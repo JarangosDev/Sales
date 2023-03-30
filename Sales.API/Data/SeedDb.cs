1d34dc1232d561111f053a43e6690be06d05a830
Jarangos <94191961+Jarangos31@users.noreply.github.com>
2023-03-30 01:02:01 -0500

    Finish page 145.

--- Sales.API/Data/SeedDb.cs
+++ Sales.API/Data/SeedDb.cs
@@ -28,7 +28,7 @@ namespace Sales.API.Data
             await CheckCountriesAsync();
             await CheckCategoriesAsync();
             await CheckRolesAsync();
-            await CheckUserAsync("1010", "Jair", "Arango", "jarangosdev@yopmail.com", "320 504 4457", "Calle Luna Calle Sol", "JuanZuluaga.jpeg", UserType.Admin);
+            await CheckUserAsync("1010", "Jair", "Arango", "jarangosdev@yopmail.com", "320 504 4457", "Calle Luna Calle Sol", "JairArango.jpg", UserType.Admin);
             await CheckUserAsync("2020", "Ledys", "Bedoya", "ledys@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "LedysBedoya.jpeg", UserType.User);
             await CheckUserAsync("3030", "Brad", "Pitt", "brad@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "Brad.jpg", UserType.User);
             await CheckUserAsync("4040", "Angelina", "Jolie", "angelina@yopmail.com", "322 311 4620", "Calle Luna Calle Sol", "Angelina.jpg", UserType.User);
